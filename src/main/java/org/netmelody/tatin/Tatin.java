a75be5b6536d59ea008725c7664abae0a639b9c3
scarytom <t.denley@cantab.net>
2012-04-29 10:19:40 +0100

    tatin server

--- src/main/java/org/netmelody/tatin/Tatin.java
+++ src/main/java/org/netmelody/tatin/Tatin.java
@@ -1,19 +1,25 @@
 package org.netmelody.tatin;
 
+import java.io.IOException;
+
+import org.netmelody.tatin.server.TatinServer;
+
+import static java.lang.Integer.parseInt;
+
 public final class Tatin {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1 || !isValidPort(args[0])) {
             System.out.println("Usage: tatin port");
             System.exit(1);
         }
         
-        final int port = Integer.parseInt(args[0]);
-        System.out.printf("tatin started on port %s", port);
+        final TatinServer tatinServer = new TatinServer(parseInt(args[0]));
+        System.out.printf("tatin started on port %s", tatinServer.port());
     }
 
     private static boolean isValidPort(String portString) {
         try {
-            final int port = Integer.parseInt(portString);
+            final int port = parseInt(portString);
             return port >= 0 && port <= 65535;
         }
         catch (Exception e) {
---
+++ src/main/java/org/netmelody/tatin/server/TatinServer.java
@@ -0,0 +1,22 @@
+package org.netmelody.tatin.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import org.simpleframework.http.core.Container;
+import org.simpleframework.transport.connect.SocketConnection;
+
+public final class TatinServer {
+
+    private final InetSocketAddress socketAddress;
+
+    public TatinServer(int port) throws IOException {
+        final Container container = null;
+        final SocketConnection connection = new SocketConnection(container);
+        socketAddress = (InetSocketAddress)connection.connect(new InetSocketAddress(port));
+    }
+    
+    public final int port() {
+        return socketAddress.getPort();
+    }
+}
