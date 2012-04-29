e4266f058d782c5e1662ffa7792ccd4bf877cfd4
scarytom <t.denley@cantab.net>
2012-04-29 10:37:07 +0100

    container

---
+++ src/main/java/org/netmelody/tatin/server/TatinContainer.java
@@ -0,0 +1,40 @@
+package org.netmelody.tatin.server;
+
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.simpleframework.http.Path;
+import org.simpleframework.http.Request;
+import org.simpleframework.http.Response;
+import org.simpleframework.http.core.Container;
+
+import com.google.common.base.Optional;
+import com.google.common.io.ByteStreams;
+
+public final class TatinContainer implements Container {
+
+    private final Map<String, byte[]> state = new HashMap<String, byte[]>();
+
+    @Override
+    public void handle(Request req, Response resp) {
+        if ("GET".equals(req.getMethod())) {
+            handleGet(req, resp);
+        }
+    }
+
+    private void handleGet(Request req, Response resp) {
+        try {
+            final OutputStream outputStream = resp.getOutputStream();
+            ByteStreams.copy(ByteStreams.newInputStreamSupplier(valueAt(req.getPath())), outputStream);
+            outputStream.close();
+        }
+        catch (Exception e) {
+        }
+    }
+
+    private byte[] valueAt(final Path path) {
+        return Optional.fromNullable(state.get(path.getPath())).or(new byte[0]);
+    }
+
+}
--- src/main/java/org/netmelody/tatin/server/TatinServer.java
+++ src/main/java/org/netmelody/tatin/server/TatinServer.java
@@ -6,12 +6,13 @@ import java.net.InetSocketAddress;
 import org.simpleframework.http.core.Container;
 import org.simpleframework.transport.connect.SocketConnection;
 
+
 public final class TatinServer {
 
     private final InetSocketAddress socketAddress;
 
     public TatinServer(int port) throws IOException {
-        final Container container = null;
+        final Container container = new TatinContainer();
         final SocketConnection connection = new SocketConnection(container);
         socketAddress = (InetSocketAddress)connection.connect(new InetSocketAddress(port));
     }
